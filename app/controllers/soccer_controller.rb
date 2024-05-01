8c0c50bf119975c2f6ad1101629c4a50fd16e5e8
Scott DeVoss <scottdevoss@yahoo.com>
2024-05-01 09:18:02 -0600

    added players to show controller

--- app/controllers/soccer_controller.rb
+++ app/controllers/soccer_controller.rb
@@ -21,5 +21,11 @@ class SoccerController < ApplicationController
     json = JSON.parse(response.body, symbolize_names: true)
 
     @team = json[:response][0]
+
+    response2 = conn.get("/players?team=#{params[:id]}&league=39&season=2023")
+
+    json2 = JSON.parse(response2.body, symbolize_names: true)
+   
+    @players = json2[:response]
   end
 end
