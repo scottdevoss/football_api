ebf1cd9186d3840f95e53d57a5e7dbcf867cf5f0
Scott DeVoss <scottdevoss@yahoo.com>
2024-05-16 16:10:21 -0600

    added NFL button

--- spec/features/sports/nfl_spec.rb
+++ spec/features/sports/nfl_spec.rb
@@ -1,7 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "NFL Page" do
-  it "has an NFL page" do
+  it "has an NFL button on explore sports page", :vcr do
+    user = User.create(name: "Abraham Lincoln", email: "honestabe@gmail.com", password: "honest", password_confirmation: "honest")
     
+    visit "/"
+
+    click_button "I already have an account"
+
+    expect(current_path).to eq("/login")
+
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    fill_in :password_confirmation, with: user.password_confirmation
+
+    click_button "Log In"
+
+    expect(current_path).to eq("/users/#{user.id}")
+
+    expect(page).to have_button("Explore Sports")
+
+    click_button "Explore Sports"
+
+    expect(current_path).to eq("/sports")
+
+    click_button "NFL"
+
+    expect(current_path).to eq("/NFL")
   end
 end
